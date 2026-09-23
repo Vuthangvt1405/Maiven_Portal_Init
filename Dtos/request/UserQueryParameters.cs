279517158c26e232fe1e731393d962e18624d870
c-peter.lam <c-peter.lam@avepoint.com>
2026-09-23 17:10:57 +0700

    update: update announcement res

--- Dtos/request/UserQueryParameters.cs
+++ Dtos/request/UserQueryParameters.cs
@@ -1,16 +1,14 @@
-using Maiven_Portal_Managment.Models.Enums;
-
-namespace Maiven_Portal_Managment.Dtos.request;
-
+namespace Maiven_Portal_Managment.Dtos.request
+{
 public sealed class UserQueryParameters
 {
     public string? Name { get; set; }
 
-    public string? Email {get; set; }
+    public string? Email { get; set; }
 
-    public UserRoleFilter? Role { get; set; }
+    public int? Status { get; set; }
 
     public int PageNumber { get; set; } = 1;
 
     public int PageSize { get; set; } = 10;
-}
+}}
