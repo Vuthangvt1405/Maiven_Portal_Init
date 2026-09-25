605cb032d8f1757281f49579ace0e59636e4a1cd
Nolan <vdthang1405205@gmail.com>
2026-09-25 11:42:37 +0700

    fix: allow zero student scores

--- Dtos/request/UpdateStudentScoreRequest.cs
+++ Dtos/request/UpdateStudentScoreRequest.cs
@@ -4,6 +4,6 @@ namespace Maiven_Portal_Managment.Dtos.request;
 
 public sealed record UpdateStudentScoreRequest
 {
-    [Range(typeof(decimal), "1", "100")]
+    [Range(typeof(decimal), "0", "100")]
     public decimal Score { get; init; }
 }
